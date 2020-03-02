ba7aeaa939df8f353d31e484150fe6e4dc5403f1
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2020-03-02 15:32:22 +0200

    first commit

---
+++ src/main/java/ruslan/simakov/spring_chat_analyzer/SpringChatAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.spring_chat_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChatAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruslan/simakov/spring_chat_analyzer/SpringChatAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.spring_chat_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
